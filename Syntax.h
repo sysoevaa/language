d8320321460ac932c543ea5154cd6c9aef5fac63
Neponel <vasoknatraktroe878@gmail.com>
2023-01-30 20:40:35 +0300

    qqqqqqq

--- Syntax.h
+++ Syntax.h
@@ -69,63 +69,93 @@ class SyntaxAnalyser {
         } while (_lex[_ind].string == ',');
     }
 
+    void determinantes() {
+        if (_lex[_ind].string == "if") {
+
+        }
+        else if (_lex[_ind].string == "while") {
+
+        }
+        else if (_lex[_ind].string == "for") {
+
+        }
+        else if (_lex[_ind].string == "do") {
+            gc();
+            if (_lex[_ind].string == "while") {
+
+            }
+            throw;
+        }
+        else if (_lex[_ind].string == "while") {
+
+        }
+        else if (_lex[_ind].string == "print") {
+
+        }
+        else if (_lex[_ind].string == "get") {
+
+        }
+        else {
+            throw;
+        }
+    }
+
     void lexpression() {
+        bool def = false;
+        if (_lex[_ind].type == 7) {
+            gc();
+            if (_lex[_ind].type != 2) {
+                throw;
+            }
+            return;
+        }
         if (type()) {
+            def = true;
             gc();
         }
-        else if (_lex[_ind].type == 1) {
-            if (_lex[_ind].string == "if") {
-
-            }
-            if (_lex[_ind].string == "while") {
-
-            }
-            if (_lex[_ind].string == "for") {
-
-            }
-            if (_lex[_ind].string == "do") {
-                gc();
-                if (_lex[_ind].string == "while") {
-
-                }
+        if (_lex[_ind].type != 2) {
+            throw;
+        }
+        gc();
+        if (_lex[_ind].string == ".") {
+            gc();
+            if (_lex[_ind].type != 2) {
                 throw;
-            }
-            if (_lex[_ind].string == "while") {
-
-            }
-            if (_lex[_ind].string == "type") {
-
-            }
-            if (_lex[_ind].string == "get") {
-
             }
         }
-        if (_lex[_ind].type == 2) {
+        if (_lex[_ind].string == "(") {
+            if (def) {
+                throw;
+            }
+            parameters();
+            if (_lex[_ind].string != ")") {
+                throw;
+            }
+        }
+        else if (_lex[_ind].string == "=") {
             gc();
-            if (_lex[_ind].string == "(") {
-                parameters();
-                if (_lex[_ind].string != ")") {
-                    throw;
-                }
-            }
-            else if (_lex[_ind].string == "=") {
-                gc();
-                expression();
-            }
-            else {
-                throw;
-            }
-            if (_lex[_ind].string != ";") {
-                throw;
-            }
+            expression();
+        }
+        else {
+            throw;
+        }
+
+    }
+
+    void namepace() {
+        if (_lex[_ind].type == 1 && !type()) {
+            determinantes();
             gc();
             namepace();
             return;
         }
-    }
-
-    void namepace() {
-
+        lexpression();
+        if (_lex[_ind].string != ";") {
+            throw;
+        }
+        gc();
+        namepace();
+        return;
     }
 
     void functionDefinition() {
