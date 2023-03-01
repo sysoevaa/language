f09d61f194182a25f609d0bf2e484c40aa34b6d5
Alexandra Sysoeva <sysoeva.alexandra2005@mail.ru>
2023-03-01 16:26:33 +0300

    add tid files

--- TID.cpp
+++ TID.cpp
@@ -1,1 +1,43 @@
 #include "TID.h"
+
+void TIDElement::AddVariable(std::string id, std::string type) {
+    _id[id] = type;
+}
+
+std::string TIDElement::GetType(std::string id) {
+    if (_id.count(id) == 0) {
+        return (std::string) "UNDEFINED";
+    }
+    return _id[id];
+}
+
+TIDElement* TIDElement::GetParent() {
+    return parent;
+}
+
+TIDElement* TIDElement::NewScope() {
+    _scopes.push_back(new TIDElement);
+    _scopes.back()->parent = this;
+    return _scopes.back();
+}
+
+std::vector<std::string> FunctionDef::GetParameters() {
+    return _formal_parameters;
+}
+
+FunctionDef::FunctionDef() {
+    parent = nullptr;
+}
+
+FunctionDef* TID::AddFunction(std::string id) {
+    if (_functions.count(id) != 0) throw std::logic_error("There are two functions"
+                                                          " with the same names");
+    return _functions[id] = new FunctionDef;
+}
+
+StructDef* TID::AddStruct(std::string id) {
+    if (_structs.count(id) != 0) throw std::logic_error("There are two structures "
+                                                        "with the same name");
+    return _structs[id] = new StructDef;
+}
+
--- TID.h
+++ TID.h
@@ -4,23 +4,41 @@
 class TIDElement {
  public:
   std::string GetType(std::string id);
-  std::string AddVariable(std::string id, std::string type);
+  void AddVariable(std::string id, std::string type);
   TIDElement *GetParent();
-  void NewScope();
- private:
+  TIDElement *NewScope();
+ protected:
   std::map<std::string, std::string> _id; // variable name, type
   std::vector<TIDElement *> _scopes;
+  TIDElement *parent;
 };
 
-class FunctionDef : public TIDElement {
+class FunctionDef : TIDElement {
+ public:
+  std::vector<std::string> GetParameters();
+  FunctionDef();
  private:
-  TIDElement *_formal_parameters;
+  std::vector<std::string> _formal_parameters;
+};
 
+class StructDef : TIDElement {
+ public:
+    FunctionDef* AddMethod(std::string id);
+    void AddMember();
+    TIDElement* FindMember(std::string id);
+    FunctionDef* FindFunction(std::string id);
+ private:
+  TIDElement *_members;
+  FunctionDef *_methods;
 };
 
 class TID {
  public:
-
+  FunctionDef *AddFunction(std::string id);
+  StructDef *AddStruct(std::string id);
  private:
   std::map<std::string, FunctionDef *> _functions;
+  std::map<std::string, StructDef *> _structs;
+  TIDElement *global;
+  TIDElement *_current_tid;
 };
