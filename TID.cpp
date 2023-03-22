4f9571511dd936b9998ed55dc490aa567ff89afa
Alexandra Sysoeva <sysoeva.alexandra2005@mail.ru>
2023-03-22 20:02:02 +0300

    bug fixed

--- TID.cpp
+++ TID.cpp
@@ -57,8 +57,8 @@ TIDElement* TIDElement::AddConstructor(std::vector<std::pair<std::string, std::s
 TIDElement *TIDElement::AddMethod(std::vector<std::pair<std::string, std::string>> &id, std::string& name, std::string& ret) {
     _methods[name] = new TIDElement;
     _methods[name]->SetType(ret);
-    for (auto [nm, type] : id) {
-        _methods[name]->AddVariable(type, nm);
+    for (auto [type, nm] : id) {
+        _methods[name]->AddVariable(nm, type);
     }
     return _methods[name];
 }
