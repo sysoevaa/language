3ce3fe40b7c094afa07e68808994e934b32cb46a
Neponel <vasoknatraktroe878@gmail.com>
2023-04-14 10:01:55 +0300

    Cleaning up Code

---
+++ PolizGenerator.cpp
@@ -0,0 +1,7 @@
+//
+// Created by А ну да on 14.04.2023.
+//
+
+#include "PolizGenerator.h"
+
+struct
---
+++ PolizGenerator.h
@@ -0,0 +1,14 @@
+//
+// Created by А ну да on 14.04.2023.
+//
+
+#ifndef LANGUAGE_POLIZGENERATOR_H
+#define LANGUAGE_POLIZGENERATOR_H
+
+
+class PolizGenerator {
+
+};
+
+
+#endif //LANGUAGE_POLIZGENERATOR_H
