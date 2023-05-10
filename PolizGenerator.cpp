c0063cb07295bc98e5b3f301fd86a5e010c48749
Alexandra Sysoeva <sysoeva.alexandra2005@mail.ru>
2023-05-10 16:38:18 +0300

    add rpn

--- PolizGenerator.cpp
+++ PolizGenerator.cpp
@@ -290,6 +290,6 @@ const int CycleSetter::GetContinue() {
     return _scopeStack.top().first;
 }
 
-void CycleSetter::PushBreak(111              {
+void CycleSetter::PushBreak(PolizJump *breakToPush) {
     _scopeStack.top().second.push_back(breakToPush);
 }
